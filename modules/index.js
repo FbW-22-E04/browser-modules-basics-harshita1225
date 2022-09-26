3dd392296a5e398557c77253741c32149386c822
harshita <harshita1225@gmail.com>
2022-09-26 14:16:25 +0200

    task completed

---
+++ modules/index.js
@@ -0,0 +1,8 @@
+import { hoverHeader } from "./hover.js";
+hoverHeader();
+
+import { scrollWindow } from "./scroll.js";
+scrollWindow();
+
+import { clickImages } from "./click.js";
+clickImages();
